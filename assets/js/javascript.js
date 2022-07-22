c5f9b43ef32a36ccc474f982444547e5175ad693
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-07-22 13:57:09 +0700

    New Realease V0.1
    Changelog
    -add Frame
    -add Canvas
    -add Button User (Batu, Gunting, Kertas)
    -add first Logo for button

---
+++ assets/js/javascript.js
@@ -0,0 +1,16 @@
+var pilihan = 0;
+
+function user(pilihan){
+    if(pilihan == 1){
+        //BatuButton
+        alert("Ini tombol batu");
+    }
+    else if(pilihan == 2){
+        //GuntingButton
+        alert("Ini tombol gunting");
+    }
+    else if(pilihan == 3){
+        //KertasButton
+        alert("Ini tombol kertas");
+    }
+}
